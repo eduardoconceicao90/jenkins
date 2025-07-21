c45c1af3fd6d8b77eeae8ae935c438d30ca0b6a8
Eduardo Conceição <eduardosaconceicao@gmail.com>
2025-07-20 21:09:18 -0300

    Controller teste

---
+++ api-rest-test-jenkins/src/main/java/io/github/eduardoconceicao90/api_rest_test_jenkins/controller/TesteController.java
@@ -0,0 +1,16 @@
+package io.github.eduardoconceicao90.api_rest_test_jenkins.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/v1/testes")
+public class TesteController {
+
+    @GetMapping
+    public String teste() {
+        return "Teste realizado com sucesso!";
+    }
+
+}
